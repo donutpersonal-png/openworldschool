aa5c09b7f53265fcfbb6ba81774e779b3a1d2423
donutpersonal-png <donutpersonal@gmail.com>
2025-12-22 15:47:54 +0300

    Update darkmode.js

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,18 +1,19 @@
 const darkToggle = document.getElementById('darkModeToggle');
 
+// Apply saved mode on load
+if (localStorage.getItem('darkMode') === 'enabled') {
+  document.body.classList.add('manual-dark');
+}
+
 darkToggle.addEventListener('click', () => {
   document.body.classList.toggle('manual-dark');
+
   if (document.body.classList.contains('manual-dark')) {
     localStorage.setItem('darkMode', 'enabled');
   } else {
     localStorage.setItem('darkMode', 'disabled');
   }
 });
-
-// Apply stored preference on page load
-if (localStorage.getItem('darkMode') === 'enabled') {
-  document.body.classList.add('manual-dark');
-}
 
 
 
